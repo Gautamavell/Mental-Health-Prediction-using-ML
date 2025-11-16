b330afb6ac309cd76fb051febe14cfa167a57864
Gautamavell <vellgautam@gmail.com>
2025-11-17 00:47:50 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from api import api
+
+def create_app():
+    app = Flask(__name__, template_folder="templates")
+
+    # Register API Blueprint
+    app.register_blueprint(api)
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
